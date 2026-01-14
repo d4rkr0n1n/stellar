9a8c41369ff3b72520ab5320e9c9e7349b5ff106
Mridul Roy <midlry.mr@gmail.com>
2026-01-14 13:43:26 +0530

    Initialize Spring Boot project with basic structure and configuration files

---
+++ src/main/java/io/d4rkr0n1n/stellar/StellarApplication.java
@@ -0,0 +1,13 @@
+package io.d4rkr0n1n.stellar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StellarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StellarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/d4rkr0n1n/stellar/StellarApplicationTests.java
@@ -0,0 +1,13 @@
+package io.d4rkr0n1n.stellar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StellarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
